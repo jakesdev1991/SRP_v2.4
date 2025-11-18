16ad836d0e6aa322b93e16cbd9b4ab37c568c0f5
Jacob See <Jake.s.dev1991@gmail.com>
2025-11-18 16:23:44 +0000

    Fix Rust naked_asm error and sync TLA+ config

--- src/common.rs
+++ src/common.rs
@@ -1,14 +1,29 @@
-pub type Result<T> = std::result::Result<T, &'static str>;
+use crate::lib::{Result, Policy};
 
+#[allow(non_camel_case_types)]
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum State {
-    S0_1_DRTM_Launch, S0_2_TCB_Measure, S0_3_Silicon_Poison_Check,
-    S0_4_Peripheral_Purge, S1_0_FIDO_Quorum, S1_1_OOB_Deliver_LiveOS,
-    S1_2_PXE_Deliver_LiveOS, S2_0_PCP_Verify, S3_0_Triage,
-    S3_1_Microcode_Verify, S4_0_LiveOS_Attest, S5_0_Policy_Fetch,
-    S5_1_Flash_Verify, S5_2_Firmware_Match, S5_3_Cleanup_Evaluate,
-    S5_4_Malware_Cleanup, S6_0_Firmware_Flash, S7_0_Commit_Attest,
-    S8_0_ZTA_Publish, S9_0_Reboot, S10_0_Halt_Error,
+    S0_1_DRTM_Launch,
+    S0_2_TCB_Measure,
+    S0_3_Silicon_Poison_Check,
+    S0_4_Peripheral_Purge,
+    S1_0_FIDO_Quorum,
+    S1_1_OOB_Deliver_LiveOS,
+    S1_2_PXE_Deliver_LiveOS,
+    S2_0_PCP_Verify,
+    S3_0_Triage,
+    S3_1_Microcode_Verify,
+    S4_0_LiveOS_Attest,
+    S5_0_Policy_Fetch,
+    S5_1_Flash_Verify,
+    S5_2_Firmware_Match,
+    S5_3_Cleanup_Evaluate,
+    S5_4_Malware_Cleanup,
+    S6_0_Firmware_Flash,
+    S7_0_Commit_Attest,
+    S8_0_ZTA_Publish,
+    S9_0_Reboot,
+    S10_0_Halt_Error,
 }
 
 #[derive(Debug, Clone)]
@@ -22,9 +37,6 @@ pub struct SrpContext {
     pub firmware_matches: bool,
     pub policy: Policy,
 }
-
-#[derive(Debug, Clone)]
-pub enum Policy { DryRun, CleanupOnly, FullFlash }
 
 impl SrpContext {
     pub fn new() -> Self {
--- src/hypervisor/drtm.rs
+++ src/hypervisor/drtm.rs
@@ -1,8 +1,11 @@
-// DRTM Stub
+use core::arch::naked_asm;
+
+/// S0.1: DRTM Launch
+/// Hardware entry point.
 #[no_mangle]
 #[naked]
 pub extern "C" fn drtm_launch() -> ! {
     unsafe {
-        core::arch::asm!("j main", options(noreturn));
+        naked_asm!("j srp_main");
     }
 }
